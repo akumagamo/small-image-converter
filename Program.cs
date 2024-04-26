41989dbbe2c4c13f57a228d002f41e68c8aaa592
Charles Lam <charles.lam@gmx.at>
2024-04-26 21:27:18 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Svg;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Xml;
+
+var configBuilder = new ConfigurationBuilder().
+   AddJsonFile("appsettings.json").Build();
+var configSection = configBuilder.GetSection("AppSettings");
+
+var inputFolder = Path.Combine(Environment.CurrentDirectory, configSection["InputFolder"] ?? "input");
+var outputFolder = Path.Combine(Environment.CurrentDirectory, configSection["OutputFolder"] ?? "output");
+
+Console.WriteLine($"Inputfolder: {inputFolder}");
+Console.WriteLine($"Outputfolder: {outputFolder}");
+
+foreach (string file in Directory.EnumerateFiles(inputFolder, "*.svg"))
+{
+    var filename = Path.GetFileName(file);
+    var svgFilename = Path.Combine(inputFolder, filename);
+    var outputSvgFilename = Path.Combine(outputFolder, filename);
+
+    var svgImage = LoadSVGImage(svgFilename);
+
+    CreateAllImages(svgImage, filename, configBuilder);
+
+    svgImage.Dispose();
+    File.Move(svgFilename, outputSvgFilename);
+    Console.WriteLine($"SVG Moved: {outputSvgFilename}");
+}
+
+Console.WriteLine("Conversion Finished!");
+
+void CreateAllImages(Bitmap svgImage, string file, IConfigurationRoot configBuilder)
+{
+    IConfigurationSection pngSizes = configBuilder.GetSection("AppSettings:PngFormats");
+    IConfigurationSection gifSizes = configBuilder.GetSection("AppSettings:GifFormats");
+
+    string filenameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+
+    if (pngSizes != null)
+    {
+        foreach (var section in pngSizes.GetChildren())
+        {
+            if (!int.TryParse(section.Value ?? "32", out int size))
+            {
+                size = 32;
+            }
+            CreateImage(svgImage, ImageFormat.Png, filenameWithoutExtension, outputFolder, size, false);
+        }
+    }
+
+    if (gifSizes != null)
+    {
+        ImageFormat currentFormat = ImageFormat.Gif;
+        foreach (var section in gifSizes.GetChildren())
+        {
+            if (!int.TryParse(section.Value ?? "32", out int size))
+            {
+                size = 32;
+            }
+            CreateImage(svgImage, ImageFormat.Gif, filenameWithoutExtension, outputFolder, size, true);
+        }
+    }
+}
+
+static void CreateImage(Bitmap baseImage, ImageFormat outputFormat, string filename, string outputfolder, int imageSize, bool isGif)
+{
+    string outputFilename = $"{filename}_{imageSize}x{imageSize}{(isGif ? ".gif" : ".png")}";
+    string outputFilenameFullpath = Path.Combine(outputfolder, outputFilename);
+    Bitmap outputImage = new Bitmap(imageSize, imageSize);
+    Graphics graphics = Graphics.FromImage(outputImage);
+    graphics.DrawImage(baseImage, 0, 0, imageSize, imageSize);
+
+    outputImage.Save(outputFilenameFullpath, outputFormat);
+    Console.WriteLine($"Created File: {outputFilenameFullpath}");
+}
+
+static Bitmap LoadSVGImage(string svgfullfilename)
+{
+    var xmlDoc = new XmlDocument
+    {
+        XmlResolver = null
+    };
+    xmlDoc.Load(svgfullfilename);
+    var xml = xmlDoc.InnerXml;
+    var svgDoc = SvgDocument.FromSvg<SvgDocument>(xml);
+    return svgDoc.Draw();
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+﻿# Mini Image Converter
+## Versionnumber 0.1.0 (2024-04-26)
+
+Small tool to create png's and gif's from an SVG.  
+All the configuration is done in the `appsettings.js`. You only can specify an input and output-folder and the desired sizes for the convered files.  
+And The application assumes the input svg ist atleat 256x256 and is a square image _(the output are also square images)_.
+
+![Screenshot from a config](https://raw.githubusercontent.com/akumagamo/small-image-converter/master/readme/screenshot_0001.png "Screenshot from a config")  
+
+## Features
+* Creates PNG and GIF version of an SVG in the output folder
+* Moves SVG after successful run into the outputfolder
+* When started will execute tasks for each SVG in the input Folder
+
+## Roadmap / Future Features
+* None
+
+## Known Bugs
+* None
+
+## SourceControl Link & Information
+https://github.com/akumagamo/small-image-converter.git
+
+## NuGet Packages used
+_for details check the Project file_
+
+    * Microsoft.Extensions.Configuration
+    * Microsoft.Extensions.Configuration.Json
+    * Svg
+
+## Config Overview   
+    {
+      "AppSettings": {
+        "InputFolder": "input",   // Inputfolder for the SVG Files
+        "OutputFolder": "output", // Outputfolder for all Images
+        "PngFormats": [ 256, 48, 32 ], // different OutputSizes for PNG's (number = width and height)
+        "GifFormats": [ 32 ] // different OutputSizes for Gif's (number = width and height)
+      }
+    }
